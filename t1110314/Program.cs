e3331f0148629c644186323b5bbd354cf38bc079
user <menmenwan46@gmail.com>
2022-03-14 09:33:46 +0800

    first

---
+++ t1110314/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace t1110314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
